f0b60352e589aa6d0600c9f24e9de03f299de119
Zitz59 <dondongudon@gmail.com>
2026-06-27 19:56:45 +0200

    feat:add console for bug search

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -111,6 +111,7 @@ export async function POST(request: Request) {
         })
         const firstBlock = msg.content[0]
         if (firstBlock && isTextBlock(firstBlock)) {
+            console.log("RAW RESPONSE",firstBlock.text)
             const textObject = JSON.parse(firstBlock.text.replace(/```([\s\S]*?)```/g, ""))
             const validatedData = dataSchema.parse(textObject)
             return NextResponse.json(validatedData)
